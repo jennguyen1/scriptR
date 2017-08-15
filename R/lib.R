2485129809dbe7e87c67c04095c1fd91e3aa1081
Jenny <jennifernguyen1992@gmail.com>
2017-08-15 15:39:47 -0400

    changed the library call to mutat_at instead of data.table

--- R/lib.R
+++ R/lib.R
@@ -19,9 +19,11 @@ install_scriptR <- function() devtools::install_github("jnguyen92/scriptR")
 #' @rdname lib
 #' @export
 lib <- function(...){
+
   args <- as.character(substitute(list(...))[-1])
-  lib_opts <- data.table::data.table(data = TRUE, develop = TRUE, viz = FALSE)[, (args) := TRUE]
+  lib_opts <- dplyr::mutate_at(dataframe(data = TRUE, develop = TRUE, viz = FALSE), dplyr::vars(dplyr::one_of(args)), function(x) TRUE)
   suppressWarnings( suppressPackageStartupMessages( import_lib(lib_opts) ) )
+
 }
 
 import_lib <- function(lib_opts){
