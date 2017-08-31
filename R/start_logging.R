71ab9efcc441ad63eb3b3584be7503fc3cb15af8
Jenny <jennifernguyen1992@gmail.com>
2017-08-30 21:53:18 -0400

    Update start_logging.R

--- R/start_logging.R
+++ R/start_logging.R
@@ -24,6 +24,7 @@ start_logging <- function(file_name = NULL){
     logging::getLogger()$addHandler(logging::writeToFile, file = logfile, level = 'DEBUG')
   }
   
+  # set level
   setLevel('DEBUG')
 
 }
