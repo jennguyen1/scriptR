74e7ea75db6f4b6af6d16161bfb94583d887da5e
Jenny <jennifernguyen1992@gmail.com>
2017-09-08 15:48:40 -0400

    add a tsv option in write.out

--- R/lib.R
+++ R/lib.R
@@ -7,7 +7,7 @@
 #'
 #' lib() imports libraries depending on args: \cr
 #'  data: (data wrangling) tidyverse, data.table, etc \cr
-#'  develop: (development) argparse, optparse, logging \cr
+#'  develop: (development) argparse, optparse, logging, parallel \cr
 #'  viz: (data visualization) ggplot2 and extensions \cr
 #'
 #' @name lib
--- R/write.out.R
+++ R/write.out.R
@@ -3,7 +3,7 @@
 #' Wrapper around write.table and write.csv depending on file extension
 #'
 #' @param dat dataframe
-#' @param file name of file
+#' @param file name of file; if file extension is csv or tsv saves file as such
 #' @param ... other parameters to pass to writing functions
 #'
 #' @export
@@ -11,8 +11,12 @@
 
 write.out <- function(dat, file, ...){
   csv <- stringr::str_detect(file, "csv$")
+  tsv <- stringr::str_detect(file, "tsv$")
+
   if(csv){
     write.csv(dat, file, row.names = FALSE, quote = FALSE, ...)
+  } else if(tsv){
+    write.table(dat, file, row.names = FALSE, quote = FALSE, sep = "\t", ...)
   } else{
     write.table(dat, file, row.names = FALSE, quote = FALSE, ...)
   }
