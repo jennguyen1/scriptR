7d7c59372d6479bbf0a4e0229817d344b789f75b
Jenny Nguyen <jennifernguyen1992@gmail.com>
2017-11-22 18:03:24 -0500

    add assertions

--- R/ensure_requisite_folders.R
+++ R/ensure_requisite_folders.R
@@ -1,8 +1,13 @@
+
 #' Creates Folder if it Doesn't Exist
 #'
-#' @param dir filepath to required folder
+#' @param path filepath to required folder
 #'
 #' @export
 #'
 
-ensure_requisite_folders <- function(dir) if( !dir.exists(dir) ) dir.create(dir, recursive = TRUE)
+ensure_requisite_folders <- function(path){
+  assertthat::assert_that(!missing(path), msg = "Input path is missing")
+  assertthat::assert_that(is.character(path))
+  if( !dir.exists(path) ) dir.create(path, recursive = TRUE)
+} 
